9fa8b1749e1ef71ad9c81177b5604892901c81fd
Chimezie Amazing <111098662+lagendary24@users.noreply.github.com>
2023-09-08 18:32:41 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,1 @@
-document.addEventListener("DOMContentLoaded", function() {
-    // Get references to the elements
-    const currentDayElement = document.querySelector("[data-testid='currentDayOfTheWeek']");
-    const currentUTCTimeElement = document.querySelector("[data-testid='currentUTCTime']");
-
-    // Get the current date and time in UTC
-    const currentDate = new Date();
-    
-    // Get the current day of the week (e.g., "Friday")
-    const currentDayOfWeek = currentDate.toLocaleDateString(undefined, { weekday: 'long' });
-
-    // Get the current time in milliseconds since January 1, 1970 (UTC)
-    const currentUTCTimeMillis = currentDate.getTime();
-
-    // Update the elements with dynamic data
-    currentDayElement.textContent = currentDayOfWeek;
-    currentUTCTimeElement.textContent = currentUTCTimeMillis + " (UTC)";
-});
+docdocument.getElementById('time').innerHTML = Date.now();
