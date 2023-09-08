ab60ac732ff5b07187c7ea6208e0372ada01b871
Chimezie Amazing <111098662+lagendary24@users.noreply.github.com>
2023-09-08 16:23:11 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,13 +3,16 @@ document.addEventListener("DOMContentLoaded", function() {
     const currentDayElement = document.querySelector("[data-testid='currentDayOfTheWeek']");
     const currentUTCTimeElement = document.querySelector("[data-testid='currentUTCTime']");
 
-    // Get the current date and time
+    // Get the current date and time in UTC
     const currentDate = new Date();
-    const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-    const currentDayOfWeek = daysOfWeek[currentDate.getUTCDay()];
-    const currentUTCTimeMillis = currentDate.getTime(); // Get time in milliseconds
+    
+    // Get the current day of the week (e.g., "Friday")
+    const currentDayOfWeek = currentDate.toLocaleDateString(undefined, { weekday: 'long' });
+
+    // Get the current time in milliseconds since January 1, 1970 (UTC)
+    const currentUTCTimeMillis = currentDate.getTime();
 
     // Update the elements with dynamic data
-    currentDayElement.textContent = `Current Day of the Week: ${currentDayOfWeek}`;
-    currentUTCTimeElement.textContent = `Current UTC Time: ${currentDate.toUTCString()} (${currentUTCTimeMillis} milliseconds)`;
+    currentDayElement.textContent = currentDayOfWeek;
+    currentUTCTimeElement.textContent = currentUTCTimeMillis + " milliseconds UTC";
 });
